73e32b864cb76778adfc853c38999f492ff8bd73
SatchitSharma <32343942+SatchitSharma@users.noreply.github.com>
2017-10-31 14:52:10 -0600

    Add files via upload

---
+++ PlayerTwo.java
@@ -0,0 +1,13 @@
+public class PlayerTwo {
+   private String player2name;
+    private int Brains;
+    public void enterName (String PlayerTwo)
+    {
+        player2name= PlayerTwo;
+
+    }
+    public void addBrains(int moreBrains){
+        Brains= Brains + moreBrains;
+    }
+}
+
